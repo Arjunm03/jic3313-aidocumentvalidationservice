ad29fc85ee1a75a92f37625864823642e67e804d
Zou, James <jameszou908@gmail.com>
2024-03-10 14:36:05 -0400

    Initialize testing module and implement log in form test

---
+++ client/src/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import axios from "axios";
+import App from "./App";
+
+jest.mock("axios");
+
+describe("App", () => {
+  test("renders login form when not logged in", () => {
+    render(<App />);
+    const loginForm = screen.getByText("Microsoft OpenAI Document Validation Service Login");
+    expect(loginForm).toBeInTheDocument();
+  });
+
+  
+});
