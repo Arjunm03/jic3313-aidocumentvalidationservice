3c9b9e5d8777025c11c98cb62eab6b28981ab303
Arjunm03 <mannana2532@gmail.com>
2024-04-01 14:06:05 -0400

    added dropdown frontend

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import { useContext, createContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-
 //START FUNCTIONS FOR LOGIN
 const authContext = createContext();
 
@@ -58,7 +57,6 @@ function useProvideAuth() {
 
 //END FUNCTIONS FOR LOGIN
 
-
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
   import.meta.url
@@ -66,13 +64,14 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 
 function App() {
   const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
   const [file, setFile] = useState("");
   const [allImage, setAllImage] = useState({});
   const [pdfFile, setPdfFile] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   const [userType, setUserType] = useState("");
-  const API = "http://localhost:3001"
+  const API = "http://localhost:3001";
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -100,6 +99,7 @@ function App() {
     setPdfFile(null);
     setAllImage({});
     setTitle("");
+    setType("");
     setFile("");
     setLoggedIn(false);
   };
@@ -128,13 +128,9 @@ function App() {
     console.log(title, file);
     console.log(formData);
 
-    const result = await axios.post(
-      API + "/upload-files",
-      formData,
-      {
-        headers: { "Content-Type": "multipart/form-data" },
-      }
-    );
+    const result = await axios.post(API + "/upload-files", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
     console.log(result);
     if (result.data.status === "ok") {
       alert("Document Uploaded Successfully!");
@@ -165,7 +161,7 @@ function App() {
   // Delete a Document by ID
   const deleteDocument = async (id, title) => {
     const result = await axios.delete(`${API}/delete-doc-byid/${id}`);
-    console.log(result.data)
+    console.log(result.data);
     alert(`${title} Successfully Deleted!`);
     getPdf();
   };
@@ -175,11 +171,11 @@ function App() {
     e.preventDefault();
     const newUser = e.target.username.value;
     const newPass = e.target.password.value;
-    const result = await axios.post(
-      `${API}/create-user/${newUser}/${newPass}`
-    );
+    const result = await axios.post(`${API}/create-user/${newUser}/${newPass}`);
     if (result.data.status === "ok") {
-      alert(`New User Created!\nUsername: ` + newUser + `\nPassword: ` + newPass);
+      alert(
+        `New User Created!\nUsername: ` + newUser + `\nPassword: ` + newPass
+      );
     } else {
       alert("Error: Username already exists.");
     }
@@ -190,7 +186,7 @@ function App() {
   // Get the Results for a PDF Validation (TODO)
   const overrideValidationResults = async (id, pdf, title, status) => {
     const pdfDir = `${API}/files/${pdf}`;
-    if (status == "Accept"){
+    if (status == "Accept") {
       const stat = "Reject";
       const description = "High";
       const result = await axios.put(
@@ -198,7 +194,10 @@ function App() {
       );
       console.log(result.data);
       alert(
-        result.data.title + ` has been overriden to ` + result.data.validationStatus + `ed.`
+        result.data.title +
+          ` has been overriden to ` +
+          result.data.validationStatus +
+          `ed.`
       );
     } else if (status == "Reject") {
       const stat2 = "Accept";
@@ -208,155 +207,202 @@ function App() {
       );
       console.log(result.data);
       alert(
-        result.data.title + ` has been overriden to ` + result.data.validationStatus + `ed.`
+        result.data.title +
+          ` has been overriden to ` +
+          result.data.validationStatus +
+          `ed.`
       );
     }
     getPdf();
   };
 
-  function ResultsPage({ location, userType, data, API}) {
+  function ResultsPage({ location, userType, data, API }) {
     // Check if location or location.state is undefined
     //if (!location || !location.state) {
-      //return <div>No data passed.</div>;
+    //return <div>No data passed.</div>;
     //}
-  
+
     //const data = location.state.data;
     // Display the results here
-  
+
     const [result, setResult] = useState(null);
-  
+
     useEffect(() => {
-  
-      const getData = async() => {
-        const val = await axios.get(
-          `${API}/validation-data/${data._id}`
-        );
+      const getData = async () => {
+        const val = await axios.get(`${API}/validation-data/${data._id}`);
         setResult(val);
-      }
+      };
       getData();
-    }, [])
-  
+    }, []);
+
     // console.log(result.data.validationStatus);
     // const result = await axios.get(
     //   `${API}/validation-data/${data._id}`
     // );
     return (
       <>
-        <div>Results of Automatic Validation: {result ? result.data.validationStatus : "Processing..."}</div>
-        {userType == "admin" && 
-        <>
-        <div> Confidence: {result ? result.data.validationDescription : "Processing..."} </div>
-        <button
-          className="btn btn-primary"
-          onClick={() => overrideValidationResults(result.data._id, result.data.pdf, result.data.title, result.data.validationStatus)}
-          >
-            Override result
-          </button>{" "}
-        </>
-        }
+        <div>
+          Results of Automatic Validation:{" "}
+          {result ? result.data.validationStatus : "Processing..."}
+        </div>
+        {userType == "admin" && (
+          <>
+            <div>
+              {" "}
+              Confidence:{" "}
+              {result
+                ? result.data.validationDescription
+                : "Processing..."}{" "}
+            </div>
+            <button
+              className="btn btn-primary"
+              onClick={() =>
+                overrideValidationResults(
+                  result.data._id,
+                  result.data.pdf,
+                  result.data.title,
+                  result.data.validationStatus
+                )
+              }
+            >
+              Override result
+            </button>{" "}
+          </>
+        )}
       </>
-    )
-  }  
+    );
+  }
 
   if (loggedIn) {
     return (
-      <div className = "container">
-      <Router>
-      <div class="App">
-        <form className="formStyle" onSubmit={submitImage}>
-          <h4>Upload PDF for Document Validation Service</h4>
-          <br />
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter Title Here"
-            required
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <br />
-          <input
-            type="file"
-            class="form-control"
-            accept="application/pdf"
-            required
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-          <br />
-          <button class="btn btn-primary" type="submit">
-            Upload File
-          </button>
-        </form>
-        <br></br>
-        <div className="uploaded">
-          <h4>Previously Uploaded PDFs:</h4>
-          <div className="output-div">
-            {Object.keys(allImage).length === 0
-              ? ""
-              : allImage.map((data, key) => {
-                  return (
-                    <><div className="inner-div">
-                      <h6>
-                        {data.title} &nbsp; &nbsp; &nbsp; &nbsp;
-                        <button
-                          className="btn btn-primary"
-                          onClick={() => showPdf(data.pdf)}
-                        >
-                          Open {data.title}
-                        </button>{" "}
-                        &nbsp; &nbsp; &nbsp; &nbsp;
-                        <button
-                          className="btn btn-secondary"
-                          onClick={() => updateValidationResults(data._id, data.pdf, data.title)}
-                          disabled={userType == "user"}
-                        >
-                          Process Document
-                        </button>{" "}
-                        &nbsp; &nbsp; &nbsp; &nbsp; Validation Status: &nbsp;
-                        <span class="badge text-bg-info">
-                          {data.validationStatus}
-                        </span>
-                        &nbsp; &nbsp; &nbsp; &nbsp;
-                        <Link
-                           to={{
-                            pathname: `/results_${key}`,
-                            state: { data: data }
-                          }}
-                           className="btn btn-secondary"
-                        >
-                         View Results
-                        </Link>
-                        &nbsp; &nbsp; &nbsp; &nbsp;
-                        <button
-                          className="btn btn-secondary"
-                          onClick={() => deleteDocument(data._id, data.title)}
-                        >
-                          Delete Document
-                        </button>
-                      </h6>
-                    </div>
-                    <Routes>
-                      <Route path={`/results_${key}`} element={<ResultsPage userType={userType} data={data} API={API}/>} />
-                    </Routes>
-                    </>
-                  );
-                })}
+      <div className="container">
+        <Router>
+          <div class="App">
+            <form className="formStyle" onSubmit={submitImage}>
+              <h4>Upload PDF for Document Validation Service</h4>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Title Here"
+                required
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <br />
+              <input
+                type="file"
+                class="form-control"
+                accept="application/pdf"
+                required
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+              <br />
+              <label for="type">Choose a document type: </label>
+              <select
+                className="type"
+                required
+                onChange={(e) => setType(e.target.value)}
+              >
+                <option value="Tax W-2">Tax W-2</option>
+                <option value="Tax 1040">Tax 1040</option>
+                <option value="Tax 1098">Tax 1098</option>
+                <option value="Tax 1099">Tax 1099</option>
+                <option value="SF-86">SF-86</option>
+              </select>
+              <button class="btn btn-primary" type="submit">
+                Upload File
+              </button>
+            </form>
+            <br></br>
+            <div className="uploaded">
+              <h4>Previously Uploaded PDFs:</h4>
+              <div className="output-div">
+                {Object.keys(allImage).length === 0
+                  ? ""
+                  : allImage.map((data, key) => {
+                      return (
+                        <>
+                          <div className="inner-div">
+                            <h6>
+                              {data.title} &nbsp; &nbsp; &nbsp; &nbsp;
+                              <button
+                                className="btn btn-primary"
+                                onClick={() => showPdf(data.pdf)}
+                              >
+                                Open {data.title}
+                              </button>{" "}
+                              &nbsp; &nbsp; &nbsp; &nbsp;
+                              <button
+                                className="btn btn-secondary"
+                                onClick={() =>
+                                  updateValidationResults(
+                                    data._id,
+                                    data.pdf,
+                                    data.title
+                                  )
+                                }
+                                disabled={userType == "user"}
+                              >
+                                Process Document
+                              </button>{" "}
+                              &nbsp; &nbsp; &nbsp; &nbsp; Validation Status:
+                              &nbsp;
+                              <span class="badge text-bg-info">
+                                {data.validationStatus}
+                              </span>
+                              &nbsp; &nbsp; &nbsp; &nbsp;
+                              <Link
+                                to={{
+                                  pathname: `/results_${key}`,
+                                  state: { data: data },
+                                }}
+                                className="btn btn-secondary"
+                              >
+                                View Results
+                              </Link>
+                              &nbsp; &nbsp; &nbsp; &nbsp;
+                              <button
+                                className="btn btn-secondary"
+                                onClick={() =>
+                                  deleteDocument(data._id, data.title)
+                                }
+                              >
+                                Delete Document
+                              </button>
+                            </h6>
+                          </div>
+                          <Routes>
+                            <Route
+                              path={`/results_${key}`}
+                              element={
+                                <ResultsPage
+                                  userType={userType}
+                                  data={data}
+                                  API={API}
+                                />
+                              }
+                            />
+                          </Routes>
+                        </>
+                      );
+                    })}
+              </div>
+              <br />
+            </div>
+            <br></br>
+            <h4>PDF Viewer</h4>
+            <PdfComp pdfFile={pdfFile} />
+            <br></br>
+            <button className="btn btn-danger" onClick={() => showPdf(null)}>
+              Close PDF Viewer
+            </button>
+            <br></br>
+            <div>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
           </div>
-          <br />
-        </div>
-        <br></br>
-        <h4>PDF Viewer</h4>
-        <PdfComp pdfFile={pdfFile} />
-        <br></br>
-        <button className="btn btn-danger" onClick={() => showPdf(null)}>
-          Close PDF Viewer
-        </button>
-        <br></br>
-        <div>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
+        </Router>
       </div>
-      </Router>
-      </div> 
     );
   } else {
     return (
